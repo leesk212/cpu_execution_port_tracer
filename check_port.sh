2b24b45cd33895012d86b3cf14ba5cdea2208d0b
leesk212 <leesk212@gmail.com>
2022-02-11 19:07:52 +0900

    init_stat

--- check_port.sh
+++ check_port.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-#StartTime=$(date +%s)
 number=0
+flag=$1
 
 echo "MSR Driver loading"
 sudo ./Driver_linux/install.sh > log.txt
 
+echo "Trace Execution port"
+
+if [ ${flag} -eq 1 ]
+then
+	echo "Execution 0,1,2,3 port" 
+	./pmc_test2 startcounters 150 151 152 153
+elif [ ${flag} -eq 2 ]
+then
+	echo "Execution 4,5,6,7 port"
+	./pmc_test2 startcounters 154 155 156 157
+fi
 
 echo "Detection Start"
 
