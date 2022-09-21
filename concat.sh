5607cf9fbf9145d5f9049918b975f03704fe3e07
leesk212 <leesk212@gmail.com>
2022-09-21 17:10:23 +0900

    seaborn

--- concat.sh
+++ concat.sh
@@ -5,27 +5,27 @@ do
     echo "Tracking "
     taskset -c 0,12 ./monitering_1
     taskset -c 0,12 ./monitering_2
-    #taskset -c 1,13 ./monitering_1
-    #taskset -c 1,13 ./monitering_2
-    #taskset -c 2,14 ./monitering_1
-    #taskset -c 2,14 ./monitering_2
-    #taskset -c 3,15 ./monitering_1
-    #taskset -c 3,15 ./monitering_2
-    #taskset -c 4,16 ./monitering_1
-    #taskset -c 4,16 ./monitering_2
-    #taskset -c 5,17 ./monitering_1
-    #taskset -c 5,17 ./monitering_2
-    #taskset -c 6,18 ./monitering_1
-    #taskset -c 6,18 ./monitering_2
-    #taskset -c 7,19 ./monitering_1
-    #taskset -c 7,19 ./monitering_2
-    #taskset -c 8,20 ./monitering_1
-    #taskset -c 8,20 ./monitering_2
-    #taskset -c 9,21 ./monitering_1
-    #taskset -c 9,21 ./monitering_2
-    #taskset -c 10,22 ./monitering_1
-    #taskset -c 10,22 ./monitering_2
-    #taskset -c 11,23 ./monitering_1
-    #taskset -c 11,23 ./monitering_2
+    taskset -c 1,13 ./monitering_1
+    taskset -c 1,13 ./monitering_2
+    taskset -c 2,14 ./monitering_1
+    taskset -c 2,14 ./monitering_2
+    taskset -c 3,15 ./monitering_1
+    taskset -c 3,15 ./monitering_2
+    taskset -c 4,16 ./monitering_1
+    taskset -c 4,16 ./monitering_2
+    taskset -c 5,17 ./monitering_1
+    taskset -c 5,17 ./monitering_2
+    taskset -c 6,18 ./monitering_1
+    taskset -c 6,18 ./monitering_2
+    taskset -c 7,19 ./monitering_1
+    taskset -c 7,19 ./monitering_2
+    taskset -c 8,20 ./monitering_1
+    taskset -c 8,20 ./monitering_2
+    taskset -c 9,21 ./monitering_1
+    taskset -c 9,21 ./monitering_2
+    taskset -c 10,22 ./monitering_1
+    taskset -c 10,22 ./monitering_2
+    taskset -c 11,23 ./monitering_1
+    taskset -c 11,23 ./monitering_2
     ((cnt++))
 done
--- tocsv_all.py
+++ tocsv_all.py
@@ -5,55 +5,60 @@ import sys
 
 temp_array = np.empty((0,10000),int)
 
-times = []
-
-uop_0 = []
-uop_1 = []
-uop_2 = []
-uop_3 = []
-uop_4 = []
-uop_5 = []
-uop_6 = []
-uop_7 = []
-
-uops = [uop_0, uop_1, uop_2, uop_3,uop_4,uop_5,uop_6,uop_7]
+core_0 = [[],[],[],[],[],[],[],[]]
+core_1 = [[],[],[],[],[],[],[],[]]
+core_2 = [[],[],[],[],[],[],[],[]]
+core_3 = [[],[],[],[],[],[],[],[]]
+core_4 = [[],[],[],[],[],[],[],[]]
+core_5 = [[],[],[],[],[],[],[],[]]
+core_6 = [[],[],[],[],[],[],[],[]]
+core_7 = [[],[],[],[],[],[],[],[]]
+core_8 = [[],[],[],[],[],[],[],[]]
+core_9 = [[],[],[],[],[],[],[],[]]
+core_10 = [[],[],[],[],[],[],[],[]]
+core_11 = [[],[],[],[],[],[],[],[]]
+cores = [core_0,core_1,core_2,core_3,core_4,core_5,core_6,core_7,core_8,core_9,core_10,core_11]
 
 for time in range(0,10000):
-    times.append(time) 
+    #times.append(time) 
     FILE_NAME = '/home/csl/Desktop/test_cpu_execution_port_tracer/cpu_execution_port_tracer/monitering_result/'+str(time)+'.txt'
     fd = open(FILE_NAME)
     lines = fd.readlines()
 
-    Datas = lines[1].strip().split(" ")
-    Data = []
-    for data in Datas:
-        if len(data) > 0:
-            Data.append(data)
-    print(Data)
-    #exit() 
-    uop_0.append(int(Data[0]))
-    uop_1.append(int(Data[1]))
-    uop_2.append(int(Data[2]))
-    uop_3.append(int(Data[3]))
-    uop_4.append(int(Data[4]))
-    uop_5.append(int(Data[5]))
-    uop_6.append(int(Data[6]))
-    uop_7.append(int(Data[7]))
+    for i,line in enumerate(lines):
+        if i > 0:
+            Datas = line.strip().split(" ")
 
-
-temp_array = np.append(temp_array,np.array([uop_0]),axis=0)
-temp_array = np.append(temp_array,np.array([uop_1]),axis=0)
-temp_array = np.append(temp_array,np.array([uop_2]),axis=0)
-temp_array = np.append(temp_array,np.array([uop_3]),axis=0)
-temp_array = np.append(temp_array,np.array([uop_4]),axis=0)
-temp_array = np.append(temp_array,np.array([uop_5]),axis=0)
-temp_array = np.append(temp_array,np.array([uop_6]),axis=0)
-temp_array = np.append(temp_array,np.array([uop_7]),axis=0)
+            Data = []
+            for data in Datas:
+                if len(data) > 0:
+                    Data.append(int(data))
+            #Data is values of each execution port of each core
+            for j in range(8):
+                cores[i-1][j].append(Data[j])
+#print(cores)
+# tocsv_all (list to numpy)
+for i,core_index in enumerate(range(len(cores))):
+    print(str(i))
+    for uop_index in range(8):
+        print('before',end=' ')
+        print(len(cores[core_index][uop_index]))
+        if len(cores[core_index][uop_index]) <  10000:
+            lack = 10000 - len(cores[core_index][uop_index])
+            print(lack)
+            for i in range(lack):
+                cores[core_index][uop_index].append(cores[core_index][uop_index][10000-lack-1])
+        print('after',end=' ')
+        print(len(cores[core_index][uop_index]))
+        temp_array = np.append(temp_array,np.array([cores[core_index][uop_index]]),axis=0)
 
 temp_array =temp_array.T
 print(temp_array.shape)
 
-df = pd.DataFrame(temp_array)
+col = []
+for k in range(12):
+    for p in range(8):
+        col.append('core_'+str(k)+'_port_'+str(p))
+
+df = pd.DataFrame(temp_array,columns=col)
 df.to_csv(sys.argv[1]+'.csv')
-
-
